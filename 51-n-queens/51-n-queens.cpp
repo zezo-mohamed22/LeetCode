ce08abd070f8f23f4e56c3eb49343fb22a5e3132
zezo-mohamed22 <72879306+zezo-mohamed22@users.noreply.github.com>
2022-06-04 02:55:12 +0200

    Time: 3 ms (96.12%), Space: 7.5 MB (49.05%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    const int MAX = 22;
+    vector<string> grid;
+    vector<vector<string>> solve;
+    vector<bool>vis,vis_right,vis_left; 
+    int n = 8, mx = 0;
+void print_Nqueens(int r)
+{
+    if (r == n)
+    {
+        solve.push_back(grid);
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i] && !vis_left[i + r] && !vis_right[n + r - i])
+        {
+            vis_right[n + r - i] = vis[i] = vis_left[i + r] = 1; // mark
+            grid[r][i] = 'Q';
+            print_Nqueens(r + 1);
+            grid[r][i] = '.';
+            vis_right[n + r - i] = vis[i] = vis_left[i + r] = 0;
+        }
+    }
+}
+    vector<vector<string>> solveNQueens(int x) {
+       n=x;
+
+        grid = vector<string>(n, string(n, '.'));
+    vis=vis_right=vis_left=vector<bool>(2*MAX+1);
+    print_Nqueens(0);
+
+        return solve;
+    }
+};
