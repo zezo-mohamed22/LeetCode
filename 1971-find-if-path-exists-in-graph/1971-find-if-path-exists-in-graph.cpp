e4db6777eef66f0413d98fd3a77cd8c72236fe4d
Abd El-Aziz Mohamed <72879306+zezo-mohamed22@users.noreply.github.com>
2022-12-19 21:37:18 +0200

    Time: 884 ms (74.71%), Space: 145.8 MB (82.17%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& v, int source, int destination) {
-        vector<int>adj[(int)2e5+5];
-        for (int i = 0 ; i <v.size(); i++){
-              adj[v[i][0]].push_back(v[i][1]);
-        adj[v[i][1]].push_back(v[i][0]);
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+          vector<int>adj[(int)2e5+5];
+        for (int i = 0 ; i <edges.size(); i++){
+              adj[edges[i][0]].push_back(edges[i][1]);
+        adj[edges[i][1]].push_back(edges[i][0]);
         }
-        if(source==destination)
-            return 1 ; 
-    vector<int> distances(n+5, -1);
-    distances[source] = 0;
+        queue<int>q;
+        q.push(source);
+        map<int,int>mp;
+        mp[source]=1;
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            if(it==destination)return true ;
+            for (auto i : adj[it]){
+            if (mp.find(i)==mp.end()){
+                
+q.push(i);
+                mp[i]=1;
+        }
 
-    queue<int> q;
-    q.push(source);
-
-    while(!q.empty())
-    {
-        int node = q.front();   q.pop();
-        if(node==destination)
-            return true; 
-             for(int child: adj[node])  
-               if(distances[child]== -1)
-            {
-                q.push(child);
-                distances[child] = 1;
-            }
     }
-
-    return 0;}   
+            } 
+    return false ;
+    }
 };
