fbaa05ff09920d5fe12a287e41a17b7765c97e44
Abd El-Aziz Mohamed <72879306+zezo-mohamed22@users.noreply.github.com>
2022-12-19 22:22:35 +0200

    Time: 9 ms (5.03%), Space: 7.9 MB (26.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-    int dp[101][101];
+    int dp[101];
     vector<int>cost ; 
     int n ;
-    int solve(int i, int lst) {
+    int solve(int i) {
     if (i >= n)return 0;
-    int &ret = dp[i][lst];
+    int &ret = dp[i];
     if (~ret)return ret;
-    ret = 0;
- if (!i && lst == n + 1) {
-        ret = max(ret, solve(i + 1, n + 1));
-        ret = max(ret, solve(i + 2, i) + cost[i]);
-    } else if (i == lst + 1) {
-        ret = max(ret, solve(i + 1, lst));
-        ret = max(ret, solve(i + 2, lst));
-    } else {
-      ret = max(ret, solve(i + 1, lst));
-        ret = max(ret, solve(i + 1, i) + cost[i]);
-        ret = max(ret, solve(i + 2, i) + cost[i]);
-    }
+    ret = max(solve(i + 1), solve(i + 2) + cost[i]);
     return ret;
+        
 }
     int rob(vector<int>& nums) {
     memset(dp, -1, sizeof dp);
     cost = nums;
         n=cost.size();
-    return solve(0,n+1);
+    return solve(0);
     }
 };
