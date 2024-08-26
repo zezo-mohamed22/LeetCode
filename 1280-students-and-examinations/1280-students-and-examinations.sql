3b75b27ac32b633d1b5e0c8eabea790e91df5a70
Abd El-Aziz Mohamed <72879306+zezo-mohamed22@users.noreply.github.com>
2024-08-26 06:16:09 +0300

    Time: 1107 ms (6.39%), Space: 0B (100.00%) - LeetHub

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,21 @@
+SELECT 
+    S.student_id, 
+    S.student_name, 
+    subj.subject_name,
+    COUNT(E.subject_name) AS attended_exams
+FROM 
+    Students S
+CROSS JOIN 
+    Subjects subj
+LEFT JOIN 
+    Examinations E
+ON  
+    S.student_id = E.student_id 
+    AND subj.subject_name = E.subject_name
+GROUP BY 
+    S.student_id, 
+    S.student_name, 
+    subj.subject_name
+ORDER BY 
+    S.student_id ASC, 
+    subj.subject_name ASC;
